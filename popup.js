00ce28da9caeea643d1cad4451a4b03c979a9cab
Tarilov Andrei <0567834@mail.ru>
2023-04-29 23:54:36 +0300

    Added variable "Works"

--- popup.js
+++ popup.js
@@ -3,8 +3,19 @@ const chatId = "271098595";
 const text = "Work Hard Play Hard!";
 const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`;
 const firstKnopa = document.getElementById("firstKnopa");
+let worked = false;
 
 firstKnopa.addEventListener("click", () => {
+  worked = !worked;
+  if (worked === true) {
+    firstKnopa.textContent = "worked";
+    sendMessage();
+  } else {
+    firstKnopa.textContent = "NO worked";
+  }
+});
+
+let sendMessage = () => {
   fetch(url, {
     method: "POST",
     headers: {
@@ -18,4 +29,4 @@ firstKnopa.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.error(error));
-});
+};
