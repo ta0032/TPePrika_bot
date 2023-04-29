f539d9cbe2afa0adce4305803fa71815c9b111c5
Tarilov Andrei <0567834@mail.ru>
2023-04-29 23:11:49 +0300

    Sends a message when the button is clicked

--- popup.js
+++ popup.js
@@ -3,30 +3,19 @@ const chatId = "271098595";
 const text = "Work Hard Play Hard!";
 const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`;
 const firstKnopa = document.getElementById("firstKnopa");
-const notificationId = null;
-let work = false;
 
 firstKnopa.addEventListener("click", () => {
-  work = !work;
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: text,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
 });
-if ((work = true)) {
-  function handleNotification(notificationId) {
-    // Установка обработчика на событие появления уведомлений
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: text + notificationId,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }
-  chrome.notifications.onShown.addListener(handleNotification);
-} else {
-  console.log("Лошара");
-}
